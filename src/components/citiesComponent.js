9dde22f86d16e2b7d601cd537258f2a3d77b1939
vrworkers <venu.mallik@gmail.com>
2023-06-21 19:14:07 +0530

    Improve cities Plot with tooltip

--- src/components/citiesComponent.js
+++ src/components/citiesComponent.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Col, Row, Select, Layout, Table, Menu, Tag } from 'antd';
+import { Col, Row, Select, Layout, Table, Menu, Tag, InputNumber, Tooltip } from 'antd';
 import { cities } from '../components/indiacities';
 import { getDistanceFromLatLonInKm } from '@/components/utils';
 
@@ -12,7 +12,6 @@ export default function CitiesDistance() {
   const [data, setData] = useState([]);
   const [selectedCity, setselectedCity] = useState("Vijayawada");
   const [pop, setPop] = useState(200000);
-  const [plotpoints,setplotpoints] = useState(100);
   const [coor, setcoor] = useState({ lat: 16.50, lon: 80.64 })
 
   const times = [];
@@ -25,10 +24,11 @@ export default function CitiesDistance() {
       let geofeature = []
       let activeCity = cities.filter((a) => a.ascii_name === selectedCity);
       activeCity = activeCity.length > 0 ? activeCity[0] : cities[0];
-      setcoor({ lat: activeCity.coordinates.lat, lon: activeCity.coordinates.lon })
+      let tcoor = { lat: activeCity.coordinates.lat, lon: activeCity.coordinates.lon };
+      setcoor(tcoor) 
 
       map = L.map('map', {
-        center: [coor.lat, coor.lon],
+        center: [tcoor.lat, tcoor.lon],
         zoom: 5
       });
       L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -37,7 +37,7 @@ export default function CitiesDistance() {
 
 
       cities.map((rec) => {
-        let d = getDistanceFromLatLonInKm(activeCity.coordinates.lat, activeCity.coordinates.lon, rec.coordinates.lat, rec.coordinates.lon);
+        let d = getDistanceFromLatLonInKm(tcoor.lat, tcoor.lon, rec.coordinates.lat, rec.coordinates.lon);
         if (rec.population > pop)
           contain.push({
             'city1': rec.ascii_name, 'pop1': rec.population, 'pop2': activeCity.population,
@@ -47,28 +47,24 @@ export default function CitiesDistance() {
       })
 
       contain.sort((a,b) => a.distance - b.distance)
-      contain.map((b)=>{
-        geofeature.push({
-          "type": "LineString",
-          "coordinates": [[coor.lat, coor.lon], [b.lat1, b.lon1]]
-        })
+      contain.slice(0,10).map((b)=>{
+        let line = [[tcoor.lat, tcoor.lon], [b.lat1, b.lon1]];
+
+        L.polyline(line, {color: 'red'}).addTo(map).bindPopup(
+          `${b.city1}-${b.city2}-${Number(b.distance).toFixed(0)}`).openPopup();
+
+        // zoom the map to the polyline
+        //map.fitBounds(polyline.getBounds());
+
         L.circle([b.lat1 , b.lon1]
           ,{
             color: 'red',
             fillColor: '#f03',
             fillOpacity: 1/(b.pop1),
             radius: 0.0005*b.pop1
-        }).addTo(map) ;
+        }).addTo(map);
       });
       setData(contain)
-      
-      {/*L.geoJSON(geofeature, {
-        style: {
-          "color": "#eeeeee",
-          "weight": 5,
-          "opacity": 1
-        }
-      }).addTo(map);*/}
     }
 
   }, [ selectedCity, pop])
@@ -94,7 +90,8 @@ export default function CitiesDistance() {
   return ( 
             !isSSREnabled() ?
             <>   
-                <Select style={{ width: '500px' }}
+                <h4>Plot the nearest cities to a town/city, above a population threshold</h4>
+                <Select style={{ width: '500px' }} title={"Select the city from which you want to measure"}
                   placeholder={'Select City'} allowClear showSearch
                   value={selectedCity} onChange={(v) => setselectedCity(v)} >
                   {cities.map((b, _) => {
@@ -102,12 +99,15 @@ export default function CitiesDistance() {
                   })}
                 </Select>
                 <Select style={{ width: 200 }} placeholder={"Select population limit"} value={pop}
+                title={"Filter the cities by population limit"}
                   onChange={(v) => setPop(v)}>
                   {
                     [1000, 2000, 5000, 10000, 15000, 25000, 50000, 75000, 100000, 200000, 500000, 1000000, 2000000].map((b) => {
                       return <Select.Option key={b}>{b}</Select.Option>
                     })}
                 </Select>
+                
+
 
                 <div id="map" style={{ height: 600 }}></div>
                 <Table dataSource={data} onChange={onChangeTable} >
