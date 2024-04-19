e1f06ce0cbef9ccb4e8b64321dd63b93ed47b0df
vrworkers <venu.mallik@gmail.com>
2024-04-20 01:56:18 +0530

    Add integrations into the dev porfile

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,14 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208509834"></script>
+        <script async id="sntlcallusat"
+            account-id="208509834"
+            default-number="13025202689"
+            add-numbers="919885920369"
+            exclude-numbers="EXCLUDE-THESE-NUMBERS"
+            src="https://widget.sonetel.com/SonetelCallUsAt.min.js"></script>
+
       </body>
     </Html>
   )
